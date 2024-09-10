77225dfe3f3d452b333b67e4e85c751a6a73402a
ylyu <yl.yu@pulmuone.com>
2024-09-10 21:54:54 +0900

    build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,8 @@ function App() {
 
   // Express 서버에서 메시지를 받아오기
   useEffect(() => {
-    axios.get('http://localhost:5000/api/message')
+    // axios.get('http://localhost:5000/api/message')
+    axios.get('https://wet-luisa-yang-yang-253f1741.koyeb.app/')
       .then(response => {
         setMessage(response.data.message);
       })
