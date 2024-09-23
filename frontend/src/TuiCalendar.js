071d523253da435a2786440a6925d24f143d5c5c
yu yang lae <yl.yu@pulmuone.com>
2024-09-23 17:15:50 +0900

    tui

--- frontend/src/TuiCalendar.js
+++ frontend/src/TuiCalendar.js
@@ -1,35 +1,143 @@
-import React, { useCallback, useRef } from "react";
-// import { render } from "react-dom";
+import React from "react";
 
-import TUICalendar from "@toast-ui/react-calendar";
-import "tui-calendar/dist/tui-calendar.css";
-import "tui-date-picker/dist/tui-date-picker.css";
-import "tui-time-picker/dist/tui-time-picker.css";
+import Calendar from '@toast-ui/react-calendar';
+import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
+import 'tui-date-picker/dist/tui-date-picker.css';
 
-import "./index.css";
+const myTheme = {
+  common: {
+    backgroundColor: '#fff',
+    border: '1px solid #ddd',
+    gridSelection: {
+      backgroundColor: 'rgba(81, 230, 92, 0.1)',
+      border: '1px solid #515ce6',
+    },
+    dayName: {
+      color: '#222',
+    },
+    holiday: {
+      color: 'rgba(255, 64, 64, 0.8)',
+    },
+    saturday: {
+      color: 'rgba(64, 64, 255, 0.8)',
+    },
+    today: {
+      color: '#fff',
+    },
+  },
+  week: {
+    dayGrid: {
+      borderRight: 'none',
+      backgroundColor: 'rgba(81, 92, 230, 0.1)',
+    },
+    dayGridLeft: {
+      borderRight: 'none',
+      backgroundColor: 'rgba(81, 92, 230, 0.05)',
+      width: '144px',
+    },
+    weekend: {
+      backgroundColor: 'rgba(255, 64, 64, 0.05)',
+    },
+    today: {
+      color: '#e5e5e5',
+      backgroundColor: 'rgba(229, 229, 229, 0.05)',
+    },
+    gridSelection: {
+      color: 'grey',
+    },
+  },
+  month: {
+    dayExceptThisMonth: {
+      color: '#aaa',
+    },
+    holidayExceptThisMonth: {
+      color: 'oringe',
+    },
+    dayName: {
+      borderLeft: 'none',
+      backgroundColor: '#eee',
+    },
+    moreView: {
+      border: '1px solid grey',
+      boxShadow: '0 2px 6px 0 grey',
+      backgroundColor: 'white',
+      width: 320,
+      height: 200,
+    },
+    moreViewTitle: {
+      backgroundColor: 'grey',
+    },
+    weekend: {
+      backgroundColor: '#f9f9f9',
+    },
+    gridCell: {
+      footerHeight: 31,
+    },
+  },
+  
+}
 
 const start = new Date();
 const end = new Date(new Date().setMinutes(start.getMinutes() + 30));
 const schedules = [
   {
-    calendarId: "1",
-    category: "time",
-    isVisible: true,
-    title: "Study",
     id: "1",
-    body: "Test",
+    calendarId: "1",
+    title: 'TOAST UI Calendar Study',
+    category: "time", //일정 카테고리. milestone, task, allday, time
+    isVisible: true, //종일 일정여부
+    body: "Test", //일정 내용
+    dueDateClass: '', //task 일정 카테고리. 어떤 문자열도 가능
     start,
-    end
+    end,
+    state: 'Busy', //일정 상태. 바쁨(Busy), 한가함(Free) 중 하나
+    isReadOnly: true, //수정 가능한 일정 여부
+    color: '#fff', //일정 요소의 텍스트 색상
+    backgroundColor: '#ccc', //일정 요소의 배경 색상
+    customStyle: {
+      fontStyle: 'italic',
+      fontSize: '15px',
+    },
+    raw: null  //실제 일정 데이터
   },
   {
-    calendarId: "2",
-    category: "time",
-    isVisible: true,
-    title: "Meeting",
     id: "2",
+    calendarId: "2",
+    title: 'Practice',
+    category: "milestone",
+    isVisible: true,
     body: "Description",
+    dueDateClass: '',
     start: new Date(new Date().setHours(start.getHours() + 1)),
-    end: new Date(new Date().setHours(start.getHours() + 2))
+    end: new Date(new Date().setHours(start.getHours() + 2)),
+    state: 'Free',
+    isReadOnly: true,
+    color: '#fff',
+    backgroundColor: '#9e5fff',
+    customStyle: {
+      fontStyle: 'italic',
+      fontSize: '15px',
+    },
+  },
+  {
+    id: "2",
+    calendarId: "3",
+    title: 'FE Workshop',
+    category: "allday",
+    isVisible: true,
+    body: "Description",
+    dueDateClass: '',
+    start,
+    end,
+    state: 'Free',
+    isReadOnly: true,
+    color: '#fff',
+    backgroundColor: '#00a9ff',
+    customStyle: {
+      fontStyle: 'italic',
+      fontSize: '15px',
+    },
   }
 ];
 
@@ -44,7 +152,16 @@ const calendars = [
   },
   {
     id: "2",
-    name: "Company",
+    name: "디자인",
+    color: "#ffffff",
+    bgColor: "#00a9ff",
+    dragBgColor: "#00a9ff",
+    borderColor: "#00a9ff"
+  }
+  ,
+  {
+    id: "3",
+    name: "퍼블",
     color: "#ffffff",
     bgColor: "#00a9ff",
     dragBgColor: "#00a9ff",
@@ -52,115 +169,229 @@ const calendars = [
   }
 ];
 
-export default function TuiCalendar() {
-  const cal = useRef(null);
+const template = {
+  milestone(schedule) {
+    return `<span style="color:#fff;background-color: ${schedule.bgColor};">${
+            schedule.title
+    }</span>`;
+  },
+  milestoneTitle() {
+    return 'Milestone';
+  },
+  task: function(schedule) {
+    return '&nbsp;&nbsp;#' + schedule.title;
+  },
+  taskTitle: function() {
+      return '<label><input type="checkbox" />Task</label>';
+  },
+  allday(schedule) {
+    return `${schedule.title}<i class="fa fa-refresh"></i>`;
+  },
+  alldayTitle() {
+    return 'All Day';
+  },
+  time: function(schedule) {
+    return schedule.title + ' <i class="fa fa-refresh"></i>' + schedule.start;
+  },
+  
+  monthMoreTitleDate(moreTitle) {
+    const { date } = moreTitle;
 
-  const onClickSchedule = useCallback((e) => {
-    const { calendarId, id } = e.schedule;
-    const el = cal.current.calendarInst.getElement(id, calendarId);
+    return `<span>${date}</span>`;  //더 보기 팝업의 날짜를 커스터마이징
+  },
+  monthMoreClose() {
+    return ''; // 더 보기 팝업 닫기 버튼 커스텀 마이징
+  },
+  // monthGridHeader(model) {
+  //   const date = parseInt(model.date.split('-')[2], 10);
 
-    console.log(e, el.getBoundingClientRect());
-  }, []);
+  //   return `<span style='background:#ddd'>${date}</span>`; //월간뷰 셀의 헤더 영역을 커스터마이징할 수 있다. TemplateMonthGrid 객체를 파라미터로 받는다.
+  // },
+  monthGridHeaderExceed(hiddenEvents) {
+    return `<span>${hiddenEvents} more</span>`; //월간뷰 셀의 헤더 영역의 초과되는 이벤트의 갯수를 표시
+  },
+  monthDayName(model) {
+    return model.label;
+  },
+  weekDayName(model) {
+    return `<span style='background:#ddd'>${model.date}</span>&nbsp;&nbsp;<span>${model.dayName}</span>`; //주간/일간뷰의 요일을 커스터마이징
+  },
+  weekGridFooterExceed(hiddenEvents) {
+    return `+${hiddenEvents}`; //주간/일간뷰의 allday 패널의 초과된 이벤트 표시 컴포넌트를 커스터마이징
+  },
+  popupIsAllday() {
+    return '하루종일'; //이벤트 폼 팝업에서 all day 텍스트를 커스터마이징
+  },
+  popupStateFree() {
+    return 'Free'; //이벤트의 한가함(free) 상태를 커스터마이징
+  },
+  popupStateBusy() {
+    return 'Busy';
+  },
+  popupDetailState({ state }) {
+    return state || 'Busy';
+  },
+  titlePlaceholder() {
+    return '제목 추가'; //이벤트 폼 팝업에서 이벤트명의 placeholder를 커스터마이징
+  },
+  locationPlaceholder() {
+    return '위치 추가';
+  },
+  popupSave: function() {
+    return '저장'
+  },
+  popupUpdate: function() {
+    return '수정'
+  },
+  popupEdit: function() {
+    return '편집'
+  },
+  popupDelete: function() {
+    return '삭제'
+  },
+  popupDetailTitle: function() {
+    return 'https://github.com/nhn/tui.calendar/blob/calendar%402.1.3/docs/ko/apis/template.md 이벤트 상세 팝업에서 이벤트명'
+  },
+  popupDetailAttendees({ attendees = [] }) {
+    return attendees.join(', '); //이벤트 상세 팝업에서 이벤트의 참석자를 커스터마이징
+  },
+  popupDetailBody({ body }) {
+    return body; //이벤트 상세 팝업에서 이벤트의 내용을 커스터마이징
+  },
+};
 
-  const onBeforeCreateSchedule = useCallback((scheduleData) => {
-    console.log(scheduleData);
 
-    const schedule = {
-      id: String(Math.random()),
-      title: scheduleData.title,
-      isAllDay: scheduleData.isAllDay,
-      start: scheduleData.start,
-      end: scheduleData.end,
-      category: scheduleData.isAllDay ? "allday" : "time",
-      dueDateClass: "",
-      location: scheduleData.location,
-      raw: {
-        class: scheduleData.raw["class"]
-      },
-      state: scheduleData.state
+class TestCal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.calendarRef = React.createRef();
+    }
+  
+    // ---------- Instance method ---------- //
+
+    // 이전 달로 이동하는 버튼
+    handleClickPrevButton = () => {
+      const calendarInstance = this.calendarRef.current.getInstance();
+
+      calendarInstance.prev();
     };
 
-    cal.current.calendarInst.createSchedules([schedule]);
-  }, []);
+    // 다음 달로 이동하는 버튼
+    handleClickNextButton = () => {
+      const calendarInstance = this.calendarRef.current.getInstance();
 
-  const onBeforeDeleteSchedule = useCallback((res) => {
-    console.log(res);
+      calendarInstance.next();
+    };
 
-    const { id, calendarId } = res.schedule;
+    // 주간뷰 보기    ( defaultView = month 로 수정해놓았습니다 )
+    weekChangeButton = () => {
+      const calendarInstance = this.calendarRef.current.getInstance();
 
-    cal.current.calendarInst.deleteSchedule(id, calendarId);
-  }, []);
-
-  const onBeforeUpdateSchedule = useCallback((e) => {
-    console.log(e);
-
-    const { schedule, changes } = e;
-
-    cal.current.calendarInst.updateSchedule(
-      schedule.id,
-      schedule.calendarId,
-      changes
-    );
-  }, []);
-
-  function _getFormattedTime(time) {
-    const date = new Date(time);
-    const h = date.getHours();
-    const m = date.getMinutes();
-
-    return `${h}:${m}`;
-  }
-
-  function _getTimeTemplate(schedule, isAllDay) {
-    var html = [];
-
-    if (!isAllDay) {
-      html.push("<strong>" + _getFormattedTime(schedule.start) + "</strong> ");
-    }
-    if (schedule.isPrivate) {
-      html.push('<span class="calendar-font-icon ic-lock-b"></span>');
-      html.push(" Private");
-    } else {
-      if (schedule.isReadOnly) {
-        html.push('<span class="calendar-font-icon ic-readonly-b"></span>');
-      } else if (schedule.recurrenceRule) {
-        html.push('<span class="calendar-font-icon ic-repeat-b"></span>');
-      } else if (schedule.attendees.length) {
-        html.push('<span class="calendar-font-icon ic-user-b"></span>');
-      } else if (schedule.location) {
-        html.push('<span class="calendar-font-icon ic-location-b"></span>');
-      }
-      html.push(" " + schedule.title);
+      calendarInstance.changeView('week', true);
     }
 
-    return html.join("");
-  }
+    // 월간뷰 보기
+    momthChangeButton = () => {
+      const calendarInstance = this.calendarRef.current.getInstance();
 
-  const templates = {
-    time: function (schedule) {
-      console.log(schedule);
-      return _getTimeTemplate(schedule, false);
+      calendarInstance.changeView('month', true);
     }
-  };
 
-  return (
-    <div className="App">
-      <h1>Welcome to TOAST Ui Calendar</h1>
+    // 일간뷰 보기
+    handleClickDayButton = () => {
+      const calendarInstance = this.calendarRef.current.getInstance();
 
-      <TUICalendar
-        ref={cal}
-        height="1000px"
-        view="week"
-        useCreationPopup={true}
-        useDetailPopup={true}
-        template={templates}
-        calendars={calendars}
-        schedules={schedules}
-        onClickSchedule={onClickSchedule}
-        onBeforeCreateSchedule={onBeforeCreateSchedule}
-        onBeforeDeleteSchedule={onBeforeDeleteSchedule}
-        onBeforeUpdateSchedule={onBeforeUpdateSchedule}
-      />
-    </div>
-  );
+      calendarInstance.changeView('day', true);
+    }
+
+    // ---------- Event ---------- //
+
+    // week 상태에서 요일 클릭
+    handleClickDayname = (ev) => {
+        console.group('onClickDayname');
+        console.log(ev.date);
+        console.groupEnd();
+    };
+
+    beforeCreateSchedule = (ev) => {
+        console.group('onbeforeCreateSchedule');
+        console.log(ev.date);
+        console.groupEnd();
+    };
+    
+
+    render() {
+        const selectedView = 'month';     // default view
+          
+        return (
+          <>
+            <button onClick={this.handleClickDayButton}>오늘</button>
+            <button onClick={this.weekChangeButton}>Week</button>
+            <button onClick={this.momthChangeButton}>momth</button>
+            <button onClick={this.handleClickPrevButton}>이전달</button>
+            <button onClick={this.handleClickNextButton}>다음달</button>
+            
+
+            <Calendar
+                ref={this.calendarRef}
+                onClickDayname={this.handleClickDayname}
+                onbeforeCreateSchedule={this.beforeCreateSchedule}
+                height="90vh"
+                calendars={calendars}
+                disableDblClick={true}
+                disableClick={false}
+                isReadOnly={false}
+                schedules={schedules}
+                scheduleView={true}
+                taskView={false}
+                template={template}
+                theme={myTheme} // 어두운 테마 사용가능
+                timezones={[
+                  {
+                    timezoneOffset: 540,
+                    displayLabel: 'GMT+09:00',
+                    tooltip: 'Seoul'
+                  }
+                ]}
+                useFormPopup={true}
+                useDetailPopup={true}
+                useCreationPopup={true}
+                view={selectedView} // `defaultView` 옵션을 설정할 수도 있습니다.
+                week={{
+                    dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+                    showTimezoneCollapseButton: true,
+                    timezonesCollapsed: true,
+                    startDayOfWeek: 0,
+                    showNowIndicator: true,
+                    taskView: false,
+                    collapseDuplicateEvents: { //일정 겹침 가능
+                      getDuplicateEvents: (targetEvent, events) =>
+                        events
+                          .filter((event) =>
+                            event.title === targetEvent.title &&
+                            event.start.getTime() === targetEvent.start.getTime() &&
+                            event.end.getTime() === targetEvent.end.getTime()
+                          )
+                          .sort((a, b) => (a.calendarId > b.calendarId ? 1 : -1)),
+                      getMainEvent: (events) => events[events.length - 1], // events는 getDuplicateEvents()의 리턴값이다.
+                    }
+                }}
+                month={{
+                  dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+                  visibleWeeksCount: 0,
+                  workweek: false, //주말제거
+                  narrowWeekend: false, //주말 작게
+                  startDayOfWeek: 0,
+                  isAlways6Weeks: true,
+                  visibleEventCount: 6, // 각 날짜 최대치 보여주는 갯수
+                }}
+            />
+            
+
+        </>
+        );
+    }
 }
+
+export default TestCal;
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-// tul 캘린더
+// import App from "./App";
 // import FullCalendar from "./FullCalendar";
-// import TuiCalendar from "./TuiCalendar";
+import TuiCalendar from "./TuiCalendar";
+// import TuiCalendar2 from "./TuiCalendar2";
 // import ReactCalendar from "./ReactCalendar";
-import ReactBigCalendar from "./ReactBigCalendar";
+// import ReactBigCalendar from "./ReactBigCalendar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <ReactBigCalendar />
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
     {/* <FullCalendar /> */}
-    {/* <TuiCalendar /> */}
+    <TuiCalendar />
+    {/* <TuiCalendar2 /> */}
     {/* <ReactCalendar /> */}
-  </React.StrictMode>
+    {/* <ReactBigCalendar /> */}
+  </StrictMode>,
+  rootElement
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
