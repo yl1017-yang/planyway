a7586082aa3ba079f1553891aff1e93605806e5b
yu yang lae <yl.yu@pulmuone.com>
2024-10-02 16:46:47 +0900

    back

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
